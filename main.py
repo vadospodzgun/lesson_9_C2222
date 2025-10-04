0ad5f4d83a97b7acb71cd494d65ef0d8cf9d8cf7
Dmitro-ZH <zele_jj21@student.itstep.org>
2025-10-04 14:49:47 +0300

    qqqq

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import cv2
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+image_path = 'cat1.jpg'
+cat_face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+image = cv2.imread(image_path)
+cat_face = cat_face_cascade. detectMultiScale(image)
+#print(cat_face)
+cat = Image.open(image_path)
+glasses = Image.open('glasses.png')
+for (x,y,w,h) in cat_face:
+    glasses = glasses.resize((w*5, h*3))
+    cat.paste(glasses, (int(x-110), int(y-80)),glasses)
+    cat.save("cat_with_glasses.png")
+    cat_with_glasses = cv2.imread("cat_with_glasses.png")
+    cv2.imshow("Cat_with_glasses", cat_with_glasses)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # cv2.imshow("Cat", image)
+    cv2.waitKey()
